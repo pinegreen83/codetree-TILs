13f906a18850dd0a8fe1f44455484928f1e46765
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-01-23 22:48:38 +0900

    Title: 사각형 채우기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240123/사각형 채우기 3/rectangle-fill-3.cpp
+++ 240123/사각형 채우기 3/rectangle-fill-3.cpp
@@ -7,14 +7,14 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    vector<int> square(n+1);
+    vector<long long> square(n+1);
     square[0] = 1;
     square[1] = 2;
     square[2] = 7;
 
     for(int i=3; i<=n; i++)
     {
-        square[i] = ((square[i-1] * 3) % 1000000007) + ((square[i-2] - square[i-3]) % 1000000007);
+        square[i] = (square[i-1] * 3 + square[i-2] - square[i-3]) % 1000000007;   
     }
     cout << square[n];
 
