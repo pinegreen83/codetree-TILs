71f0ac3c5c220f2286aadf26e04e0a0ffbca5d09
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-02-03 22:29:21 +0900

    Title: 최대 증가 부분 수열, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240203/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    vector<int> numbers(n);
+    vector<int> dp;
+
+    for(int i=0; i<n; i++)
+    {
+        cin >> numbers[i];
+    }
+
+    dp.push_back(numbers[0]);
+    
+    int answer = 0;
+
+    for(int i=1; i<n; i++)
+    {
+        while(dp.back() > numbers[i])
+        {
+            dp.pop_back();
+        }
+        dp.push_back(numbers[i]);
+        int size = dp.size();
+        answer = max(answer, size);
+    }
+
+    cout << answer;
+
+    return 0;
+}
