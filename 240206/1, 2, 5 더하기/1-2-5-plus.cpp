317537dc24f12331a957b97ccfbb2e650b3080b8
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-02-06 14:45:58 +0900

    Title: 1, 2, 5 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240206/1, 2, 5 더하기/1-2-5-plus.cpp
+++ 240206/1, 2, 5 더하기/1-2-5-plus.cpp
@@ -10,9 +10,7 @@ int main() {
     vector<int> nums = {1, 2, 5};
 
     vector<int> dp(n+1);
-    dp[1] = 1;
-    dp[2] = 1;
-    dp[5] = 1;
+    dp[0] = 1;
     for(int i=1; i<=n; i++)
     {
         for(int j=0; j<3; j++)
