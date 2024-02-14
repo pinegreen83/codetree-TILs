ea8cb45e09ba4b264d712b2bf544363bd08932f7
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-02-14 14:52:07 +0900

    Title: 단 한 번의 2048 시도, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240214/단 한 번의 2048 시도/one-trial-of-2048-game.cpp
@@ -0,0 +1,168 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    vector<vector<int>> map(4, vector<int>(4));
+    for(int i=0; i<4; i++)
+    {
+        for(int j=0; j<4; j++)
+        {
+            cin >> map[i][j];
+        }
+    }
+
+    char dir;
+    cin >> dir;
+
+    switch(dir)
+    {
+        case 'L':
+            for(int i=0; i<4; i++)
+            {
+                vector<int> temp;
+                queue<int> q;
+                for(int j=0; j<4; j++)
+                {
+                    if(!map[i][j]) continue;
+                    else q.push(map[i][j]);
+                }
+                while(!q.empty())
+                {
+                    int now = q.front();
+                    q.pop();
+                    if(now == q.front())
+                    {
+                        now *= 2;
+                        q.pop();
+                    }
+                    temp.push_back(now);
+                }
+                while(temp.size() < 4)
+                {
+                    temp.push_back(0);
+                }
+                for(auto t : temp)
+                {
+                    cout << t << " ";
+                }
+                cout << endl;
+            }
+            break;
+        case 'R':
+            for(int i=0; i<4; i++)
+            {
+                vector<int> temp;
+                queue<int> q;
+                for(int j=3; j>=0; j--)
+                {
+                    if(!map[i][j]) continue;
+                    else q.push(map[i][j]);
+                }
+                while(!q.empty())
+                {
+                    int now = q.front();
+                    q.pop();
+                    if(now == q.front())
+                    {
+                        now *= 2;
+                        q.pop();
+                    }
+                    temp.push_back(now);
+                }
+                while(temp.size() < 4)
+                {
+                    temp.push_back(0);
+                }
+                for(int j=0; j<4; j++)
+                {
+                    cout << temp[3-j] << " ";
+                }
+                cout << endl;
+            }
+            break;
+        case 'U':
+            for(int i=0; i<4; i++)
+            {
+                vector<int> temp;
+                queue<int> q;
+                for(int j=0; j<4; j++)
+                {
+                    if(!map[j][i]) continue;
+                    else q.push(map[j][i]);
+                }
+                while(!q.empty())
+                {
+                    int now = q.front();
+                    q.pop();
+                    if(now == q.front())
+                    {
+                        now *= 2;
+                        q.pop();
+                    }
+                    temp.push_back(now);
+                }
+                while(temp.size() < 4)
+                {
+                    temp.push_back(0);
+                }
+                for(int j=0; j<4; j++)
+                {
+                    map[j][i] = temp[j];
+                }
+            }
+            for(auto m : map)
+            {
+                for(auto i : m)
+                {
+                    cout << i << " ";
+                }
+                cout << endl;
+            }
+            break;
+        case 'D':
+            for(int i=0; i<4; i++)
+            {
+                vector<int> temp;
+                queue<int> q;
+                for(int j=3; j>=0; j--)
+                {
+                    if(!map[j][i]) continue;
+                    else q.push(map[j][i]);
+                }
+                while(!q.empty())
+                {
+                    int now = q.front();
+                    q.pop();
+                    if(now == q.front())
+                    {
+                        now *= 2;
+                        q.pop();
+                    }
+                    temp.push_back(now);
+                }
+                while(temp.size() < 4)
+                {
+                    temp.push_back(0);
+                }
+                for(int j=3; j>=0; j--)
+                {
+                    map[j][i] = temp[3-j];
+                }
+            }
+            for(auto m : map)
+            {
+                for(auto i : m)
+                {
+                    cout << i << " ";
+                }
+                cout << endl;
+            }
+            break;
+    }
+
+    return 0;
+}
