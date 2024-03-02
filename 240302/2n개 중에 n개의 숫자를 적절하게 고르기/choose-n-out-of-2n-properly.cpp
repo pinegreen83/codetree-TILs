d79bf4d6e189ecd9efbd4bd88be0350a3f0684a8
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-03-02 20:36:38 +0900

    Title: 2n개 중에 n개의 숫자를 적절하게 고르기, Time: 72ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240302/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int n, sum;
+int answer = INT_MAX;
+vector<int> nums;
+
+void numdiv(int cnt, int start, int s)
+{
+    if(cnt == n)
+    {
+        int b = sum - s;
+        answer = min(answer, abs(b-s));
+        return;
+    }
+    for(int i=start+1; i<n*2; i++)
+    {
+        numdiv(cnt+1, i, s+nums[i]);
+        numdiv(cnt, i, s);
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    nums = vector<int>(n*2 + 1);
+    sum = 0;
+    for(int i=1; i<=n*2; i++)
+    {
+        cin >> nums[i];
+        sum += nums[i];
+    }
+
+    numdiv(0, 0, 0);
+    cout << answer;
+
+    return 0;
+}
