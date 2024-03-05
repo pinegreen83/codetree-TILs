4d63d51fc388c589306050ec211c8e2d1a5db74a
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-03-05 14:00:40 +0900

    Title: 두 수의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240305/두 수의 합/sum-of-two-num.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unordered_map>
+
+#define MAX_NUM 100000
+
+using namespace std;
+int n, k;
+int nums[MAX_NUM];
+unordered_map<int, int> umap;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> k;
+    int a, ans = 0;
+    for(int i=0; i<n; i++)
+    {
+        cin >> nums[i];
+        if(umap.find(nums[i]) == umap.end()) umap[nums[i]] = 1;
+        else umap[nums[i]]++;
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        if(umap.find(k-nums[i]) != umap.end()) ans += umap[k-nums[i]];
+    }
+    cout << ans/2;
+
+    return 0;
+}
