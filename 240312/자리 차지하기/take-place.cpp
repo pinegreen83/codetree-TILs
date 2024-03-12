10c98124fe7f4001a16e5a4bcd1c94d4e733190c
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-03-12 19:01:32 +0900

    Title: 자리 차지하기, Time: 75ms, Memory: 4MB, Status: Passed - CodeTree

--- 240312/자리 차지하기/take-place.cpp
+++ 240312/자리 차지하기/take-place.cpp
@@ -5,35 +5,29 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n, m, a;
+    int n, m, a, ans = 0;
     cin >> n >> m;
     set<int> s;
-    set<int>::iterator it;
 
-    bool bCanSeat = true;
+    for(int i=1; i<=m; i++)
+    {
+        s.insert(i);
+    }
+
     for(int i=0; i<n; i++)
     {
-        if(!bCanSeat) break;
         cin >> a;
-        it = s.lower_bound(a);
-        if(a != *it) s.insert(a);
-        else
+        set<int>::iterator it = s.upper_bound(a);
+
+        if(it != s.begin())
         {
-            while(it != s.begin())
-            {
-                it--;
-                if(*it < a-1)
-                {
-                    a--;
-                    break;
-                }
-                else a = *it;
-            }
-            if(a == 0) bCanSeat = false;
-            else s.insert(a);
+            it--;
+            s.erase(*it);
+            ans++;
         }
+        else break;
     }
-    cout << s.size();
+    cout << ans;
 
     return 0;
 }
