882c743973746d3f8201482296e83a81a86731dd
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-03-12 18:12:57 +0900

    Title: 자리 차지하기, Time: 29ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240312/자리 차지하기/take-place.cpp
+++ 240312/자리 차지하기/take-place.cpp
@@ -1,39 +1,26 @@
 #include <iostream>
-#include <set>
+#include <vector>
 
 using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n, m, a;
+    int n, m, a, answer = 0;
     cin >> n >> m;
+    vector<int> chairs(m+1);
 
-    set<int> s;
-    set<int>::iterator it;
     for(int i=0; i<n; i++)
     {
         cin >> a;
-        it = s.lower_bound(a);
-        if(a != *it)
-        {
-            s.insert(a);
-        }
-        else
-        {
-            if(*it == 1) break;
-            while(*it > 0)
-            {
-                if(*it < a-1)
-                {
-                    s.insert(a-1);
-                    break;
-                }
-                else a = *it;
-                it--;
-            }
-        }
+        chairs[a]++;
     }
-    cout << s.size();
+
+    for(int i=1; i<=m; i++)
+    {
+        answer += chairs[i];
+        if(answer > i) answer = i;
+    }
+    cout << answer;
 
     return 0;
 }
