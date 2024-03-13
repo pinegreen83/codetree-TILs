b6cb3e40c48c89b3d2405e67ecb72866151b8a62
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-03-13 18:10:13 +0900

    Title: 배열 추출, Time: 151ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240313/배열 추출/array-extraction.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, a;
+    cin >> n;
+    priority_queue<int> pq;
+    for(int i=0; i<n; i++)
+    {
+        cin >> a;
+        if(a == 0)
+        {
+            if(pq.empty()) cout << 0 << endl;
+            else
+            {
+                cout << pq.top() << endl;
+                pq.pop();
+            }
+        }
+        else pq.push(a);
+    }
+    return 0;
+}
