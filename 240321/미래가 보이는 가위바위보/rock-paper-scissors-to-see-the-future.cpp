3a84e2a44e1caeef468409952cbf4d53b3d2c3a5
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-03-21 19:14:23 +0900

    Title: 미래가 보이는 가위바위보, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240321/미래가 보이는 가위바위보/rock-paper-scissors-to-see-the-future.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    vector<char> hsp(n);
+    vector<char> type = {'H', 'S', 'P'};
+    for(int i=0; i<n; i++)
+    {
+        cin >> hsp[i];
+    }
+    vector<int> left(n);
+    vector<int> right(n);
+    for(int i=0; i<3; i++)
+    {
+        int now = 0;
+        for(int j=0; j<n; j++)
+        {
+            if(hsp[j] == type[i]) now++;
+            left[j] = max(left[j], now);
+        }
+
+        now = 0;
+        for(int j=n-1; j>=0; j--)
+        {
+            if(hsp[j] == type[i]) now++;
+            right[j] = max(right[j], now);
+        }
+    }
+
+    int ans = 0;
+    for(int i=0; i<n; i++)
+    {
+        ans = max(ans, left[i] + right[i]);
+    }
+    cout << ans;
+
+    return 0;
+}
