8c54c8a74f3395f581503d5d91fd10eb4b6e4e0c
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-03-21 16:49:13 +0900

    Title: 마라톤 중간에 택시타기, Time: 68ms, Memory: 6MB, Status: Passed - CodeTree

---
+++ 240321/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <climits>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    vector<vector<int>> nums(n, vector<int>(2));
+    for(int i=0; i<n; i++)
+    {
+        cin >> nums[i][0] >> nums[i][1];
+    }
+    vector<int> left(n);
+    vector<int> right(n);
+    left[0] = 0;
+    right[n-1] = 0;
+    for(int i=1; i<n; i++)
+    {
+        left[i] = left[i-1] + abs(nums[i][1] - nums[i-1][1]) + abs(nums[i][0] - nums[i-1][0]);
+    }
+    for(int i=n-2; i>=0; i--)
+    {
+        right[i] = right[i+1] + abs(nums[i][1] - nums[i+1][1]) + abs(nums[i][0] - nums[i+1][0]);
+    }
+
+    int ans = INT_MAX;
+    for(int i=1; i<n-1; i++)
+    {
+        int dist = left[i-1] + right[i+1] + abs(nums[i-1][1] - nums[i+1][1]) + abs(nums[i-1][0] - nums[i+1][0]);
+        ans = min(ans, dist);
+    }
+    cout << ans;
+
+    return 0;
+}
