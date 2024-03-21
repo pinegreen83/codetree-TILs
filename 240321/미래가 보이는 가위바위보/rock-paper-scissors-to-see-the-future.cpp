338e110405181a8be06022d0a5843c6090a4a6a6
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-03-21 19:15:16 +0900

    Title: 미래가 보이는 가위바위보, Time: 16ms, Memory: 1MB, Status: Passed - CodeTree

--- 240321/미래가 보이는 가위바위보/rock-paper-scissors-to-see-the-future.cpp
+++ 240321/미래가 보이는 가위바위보/rock-paper-scissors-to-see-the-future.cpp
@@ -34,9 +34,9 @@ int main() {
     }
 
     int ans = 0;
-    for(int i=1; i<n; i++)
+    for(int i=0; i<n-1; i++)
     {
-        ans = max(ans, left[i] + right[i]);
+        ans = max(ans, left[i] + right[i+1]);
     }
     cout << ans;
 
