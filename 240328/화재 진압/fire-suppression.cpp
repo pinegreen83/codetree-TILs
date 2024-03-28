f8fa5d596d0dca918a7ba6d713f07a74ebedf875
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-03-28 17:03:05 +0900

    Title: 화재 진압, Time: 79ms, Memory: 0MB, Status: Passed - CodeTree

--- 240328/화재 진압/fire-suppression.cpp
+++ 240328/화재 진압/fire-suppression.cpp
@@ -18,6 +18,8 @@ int main() {
     {
         cin >> firestations[i];
     }
+    sort(fires.begin(), fires.end());
+    sort(firestations.begin(), firestations.end());
 
     int j = 0;
     for(int i=0; i<n; i++)
