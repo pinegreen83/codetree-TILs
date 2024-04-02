e5cfde084031506212158d2ee19792ffd0431dd4
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-04-02 12:51:15 +0900

    Title: 동전 더하기, Time: 1016ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240402/동전 더하기/add-coins.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, k;
+    cin >> n >> k;
+    vector<int> nums(n);
+    for(int i=0; i<n; i++) cin >> nums[i];
+
+    int cnt = n-1, ans = 0;
+    while(k > 0)
+    {
+        if(k / nums[cnt])
+        {
+            ans += k / nums[cnt];
+            k = k % nums[cnt];
+            cnt--;
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
