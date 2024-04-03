79a9eeebcfcfaabb478f014198f5656aebe3c93f
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-04-03 12:25:27 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 64ms, Memory: 2MB, Status: Wrong Answer - CodeTree

--- 240403/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.cpp
+++ 240403/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.cpp
@@ -20,7 +20,7 @@ bool comp(const vector<float>& a, const vector<float>& b)
 int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n >> m;
-    nums = vector<vector<double>>(n, vector<double>(2));
+    nums = vector<vector<float>>(n, vector<float>(2));
     for(int i=0; i<n; i++)
     {
         cin >> nums[i][0] >> nums[i][1];
@@ -28,23 +28,24 @@ int main() {
 
     for(int i=0; i<n; i++)
     {
-        double val = (double) nums[i][1] / nums[i][0];
+        float val = (float) nums[i][1] / nums[i][0];
         nums[i].push_back(val);
     }
 
     sort(nums.begin(), nums.end(), comp);
-    double ans = 0;
+    float ans = 0;
     for(auto n : nums)
     {
-        if(m > n[1])
+        if(m >= n[1])
         {
             m -= n[0];
             ans += n[1];
         }
         else
         {
-            ans += ((double) m / n[0]) * n[1];
+            ans += ((float) m / n[0]) * n[1];
             m = 0;
+            break;
         }
     }
     cout << fixed;
