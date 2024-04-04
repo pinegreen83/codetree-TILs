43b6799fd9740f179572061a2d575781208a5e35
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-04-04 14:29:28 +0900

    Title: 수 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240404/수 채우기/fill-in-number.cpp
+++ 240404/수 채우기/fill-in-number.cpp
@@ -14,6 +14,7 @@ int main() {
         cnt++;
         if(n % 2 == 0) ans = min(ans, cnt + n / 2);
     }
+    if(n % 2 == 0) ans = min(ans, cnt + n / 2);
     cout << (ans == INT_MAX ? -1 : ans);
 
     return 0;
