63d08b2978559f0ff702db53667def33e8d374df
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-04-06 17:52:56 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240406/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> edges(n+1, vector<int>(n+1));
+    for(int i=0; i<m; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        edges[a][b] = c;
+    }
+    
+    vector<int> graph(n+1, INT_MAX);
+    vector<bool> visited(n+1, false);
+    graph[1] = 0;
+    for(int i=1; i<=n; i++)
+    {
+        int minidx = 0;
+        for(int j=1; j<=n; j++)
+        {
+            if(visited[j]) continue;
+
+            if(graph[minidx] > graph[j]) minidx = j;
+        }
+
+        visited[minidx] = true;
+
+        for(int j=1; j<=n; j++)
+        {
+            if(edges[minidx][j])
+            {
+                if(graph[j] != INT_MAX)
+                {
+                    graph[j] = min(graph[j], graph[minidx] + edges[minidx][j]);
+                }
+                else graph[j] = graph[minidx] + edges[minidx][j];
+            }
+        }
+    }
+    for(int i=2; i<=n; i++) cout << graph[i] << "\n";
+
+    return 0;
+}
