f7d09b68ce19f33ec486b100a3f4fc83ddfdf6ad
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-04-08 12:53:26 +0900

    Title: 최단 거리 3, Time: 49ms, Memory: 2MB, Status: Wrong Answer - CodeTree

---
+++ 240408/최단 거리 3/shortest-distance-3.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> maps(n+1, vector<int>(n+1));
+    for(int i=0; i<m; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        maps[a][b] = c;
+        maps[b][a] = c;
+    }
+
+    int start, end;
+    cin >> start >> end;
+    vector<int> nodes(n+1, INT_MAX);
+    vector<int> path(n+1);
+    vector<bool> visited(n+1);
+    nodes[start] = 0;
+    for(int i=1; i<=n; i++)
+    {
+        int minidx = 0;
+        for(int j=1; j<=n; j++)
+        {
+            if(visited[j]) continue;
+
+            if(nodes[minidx] > nodes[j])
+            {
+                minidx = j;
+            }
+        }
+
+        visited[minidx] = true;
+
+        for(int j=1; j<=n; j++)
+        {
+            if(maps[minidx][j] == 0) continue;
+
+            if(nodes[j] > nodes[minidx] + maps[minidx][j])
+            {
+                nodes[j] = nodes[minidx] + maps[minidx][j];
+                path[j] = minidx;
+            }
+        }
+    }
+    cout << nodes[end];
+
+    return 0;
+}
