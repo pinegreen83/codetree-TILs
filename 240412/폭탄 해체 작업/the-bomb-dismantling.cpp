37049d55d649dd1e54c9c62341055285dd548919
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-04-12 16:44:32 +0900

    Title: 폭탄 해체 작업, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240412/폭탄 해체 작업/the-bomb-dismantling.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    priority_queue<pair<int, int>> pq;
+    for(int i=0; i<n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        pq.push(make_pair(-b, a));
+    }
+
+    int time = 0, ans = 0;
+    while(!pq.empty())
+    {
+        if(time < -pq.top().first)
+        {
+            ans += pq.top().second;
+            pq.pop();
+        }
+        time++;
+        while(time == -pq.top().first) pq.pop();
+    }
+    cout << ans;
+
+    return 0;
+}
