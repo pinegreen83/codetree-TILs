36496e3c59d53b1564fadd7f038d7eee4e842661
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-04-16 15:52:59 +0900

    Title: 트리 파악, Time: 72ms, Memory: 4MB, Status: Wrong Answer - CodeTree

--- 240416/트리 파악/identifying-the-tree.cpp
+++ 240416/트리 파악/identifying-the-tree.cpp
@@ -1,37 +1,42 @@
 #include <iostream>
 #include <vector>
 
+#define MAX_N 100000;
+
 using namespace std;
 
 int cnt = 0;
-vector<int> parents;
+vector<int> dist;
 vector<vector<int>> child;
+
+void DFS(int start, int h)
+{
+    dist[start] = h;
+    for(int i=0; i<child[start].size(); i++)
+    {
+        DFS(child[start][i], h+1);
+    }
+}
 
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    parents = vector<int>(n+1);
+    dist = vector<int>(n+1);
     child = vector<vector<int>>(n+1);
     for(int i=1; i<n; i++)
     {
         int a, b;
         cin >> a >> b;
-        parents[b] = a;
         child[a].push_back(b);
     }
 
+    DFS(1, 0);
+
     int ans = 0;
-    for(int i=2; i<=n; i++)
+    for(int i=1; i<=n; i++)
     {
-        if(child[i].size() > 0) continue;
-        int cnt = 0, now = i;
-        while(parents[now])
-        {
-            cnt++;
-            now = parents[now];
-        }
-        ans += cnt;
+        if(child[i].size() == 0) ans += dist[i];
     }
     cout << ans%2;
 
