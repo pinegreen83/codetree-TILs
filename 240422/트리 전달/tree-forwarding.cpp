54083a529a9e201773e8499d349ae513a31f48b0
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-04-22 14:25:15 +0900

    Title: 트리 전달, Time: 135ms, Memory: 3MB, Status: Wrong Answer - CodeTree

--- 240422/트리 전달/tree-forwarding.cpp
+++ 240422/트리 전달/tree-forwarding.cpp
@@ -3,18 +3,41 @@
 
 using namespace std;
 
+vector<int> parents, dp;
+vector<vector<int>> edges;
+
+void DFS(int now)
+{
+    for(int i=0; i<edges[now].size(); i++)
+    {
+        int next = edges[now][i];
+        dp[next] += dp[now];
+        DFS(next);
+    }
+}
+
 int main() {
     // 여기에 코드를 작성해주세요.
     int n, m;
     cin >> n >> m;
-    vector<int> parents(n+1), dp(n+1);
-    for(int i=1; i<=n; i++) cin >> parents[i];
+    parents = vector<int>(n+1);
+    edges = vector<vector<int>>(n+1);
+    cin >> parents[1];
+    for(int i=2; i<=n; i++) 
+    {
+        cin >> parents[i];
+        edges[parents[i]].push_back(i);
+    }
+
+    dp = vector<int>(n+1);
     for(int i=0; i<m; i++)
     {
         int a, b;
         cin >> a >> b;
-        dp[a] = dp[parents[a]] + b;
+        dp[a] = b;
     }
+
+    DFS(1);
     for(int i=1; i<=n; i++) cout << dp[i] << " ";
 
     return 0;
