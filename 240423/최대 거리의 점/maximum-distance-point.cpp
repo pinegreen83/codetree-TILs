7f976146f47ce2fcb5d6f29319fcf97c393949f7
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-04-23 15:08:55 +0900

    Title: 최대 거리의 점, Time: 73ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240423/최대 거리의 점/maximum-distance-point.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+vector<int> nums;
+
+bool maxlen(int mid)
+{
+    int cnt = 1, len = 0, start = nums[0];
+    for(int i=1; i<n; i++)
+    {
+        int diff = nums[i] - start;
+        if(len+diff >= mid)
+        {
+            cnt++;
+            len = 0;
+        }
+        else len += diff;
+        start = nums[i];
+    }
+
+    return cnt >= m;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m;
+    nums = vector<int>(n);
+    for(int i=0; i<n; i++) cin >> nums[i];
+
+    int left = 1, right = 28, ans = 0;
+    while(left <= right)
+    {
+        int mid = (left + right) / 2;
+        if(maxlen(mid))
+        {
+            left = mid + 1; 
+            ans = max(ans, mid);
+        }
+        else right = mid - 1;
+    }
+    cout << ans;
+
+    return 0;
+}
