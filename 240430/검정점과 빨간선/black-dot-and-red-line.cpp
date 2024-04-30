edbc6ad1c8f1e42443c760635ce6209156e41084
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-04-30 15:27:49 +0900

    Title: 검정점과 빨간선, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240430/검정점과 빨간선/black-dot-and-red-line.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, a, b;
+    cin >> n >> a >> b;
+    vector<int> nums(n);
+    for(int i=0; i<n; i++) cin >> nums[i];
+    sort(nums.begin(), nums.end());
+
+    // 두 점 사이에 한 점을 둬서 선분을 그리는게 나은지 혹은 한 점에서 그냥 있는게 나은지
+    float maxr = (float) a / b;
+    int left = 0, right = 0, cnt = 0;
+    float ans = 0;
+    while(right < n)
+    {
+        if(nums[right+1] - nums[left] > maxr*2)
+        {
+            int diff = nums[right] - nums[left];
+            float r = (float) diff / 2;
+            ans += (float) a + (b * r);
+            right++;
+            left = right;
+        }
+        right++;
+    }
+    if(left == n-1) ans += a;
+    cout << ans;
+
+    return 0;
+}
