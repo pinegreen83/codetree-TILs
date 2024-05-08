de0230e0c569b1e16c4f0d7acff46547046a6e62
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-05-08 14:40:07 +0900

    Title: 경유지가 있는 경로, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240508/경유지가 있는 경로/route-with-waypoints.cpp
+++ 240508/경유지가 있는 경로/route-with-waypoints.cpp
@@ -40,13 +40,16 @@ int main() {
     for(int i=x; i<n; i++) dp[i][y] = dp[x][y];
     for(int i=y; i<m; i++) dp[x][i] = dp[x][y];
 
-    for(int i=x; i<n; i++)
+    for(int i=x+1; i<n; i++)
     {
-        for(int j=y; j<m; j++)
+        for(int j=y+1; j<m; j++)
         {
-            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            if(i == 0) dp[i][j] = dp[i][j-1];
+            else if(j == 0) dp[i][j] = dp[i-1][j];
+            else dp[i][j] = dp[i-1][j] + dp[i][j-1];
         }
     }
+
     cout << dp[n-1][m-1];
 
     return 0;
