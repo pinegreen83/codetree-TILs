2ec12dd24cd1f8c5731927e34d68c3e4f057447b
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-05-08 14:30:52 +0900

    Title: 경유지가 있는 경로, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240508/경유지가 있는 경로/route-with-waypoints.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m, k;
+vector<vector<int>> map;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m >> k;
+    map = vector<vector<int>>(n, vector<int>(m));
+
+    int cnt = 1, x = n-1, y = m-1;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            map[i][j] = cnt++;
+            if(map[i][j] == k)
+            {
+                x = i;
+                y = j;
+            }
+        }
+    }
+
+    vector<vector<int>> dp(n, vector<int>(m));
+    for(int i=1; i<=x; i++) dp[i][0] = 1;
+    for(int i=1; i<=y; i++) dp[0][i] = 1;
+
+    for(int i=1; i<=x; i++)
+    {
+        for(int j=1; j<=y; j++)
+        {
+            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        }
+    }
+
+    for(int i=x; i<n; i++) dp[i][y] = dp[x][y];
+    for(int i=y; i<m; i++) dp[x][i] = dp[x][y];
+
+    for(int i=x; i<n; i++)
+    {
+        for(int j=y; j<m; j++)
+        {
+            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        }
+    }
+    cout << dp[n-1][m-1];
+
+    return 0;
+}
