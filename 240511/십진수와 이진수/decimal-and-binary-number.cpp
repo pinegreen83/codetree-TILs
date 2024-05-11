2337f8559717e8e233caee410af7c8e421a91183
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-05-11 16:04:29 +0900

    Title: 십진수와 이진수, Time: 587ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240511/십진수와 이진수/decimal-and-binary-number.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string n;
+    cin >> n;
+
+    int a = 1, goal = 0;
+    for(int i=n.size()-1; i>=0; i--)
+    {
+        if(n[i] == '1')
+        {
+            goal += a;
+        }
+        a *= 2;
+    }
+    goal *= 17;
+    string ans, temp;
+    while(goal)
+    {
+        if(goal % 2 == 0) temp += '0';
+        else temp+= '1';
+        goal /= 2;
+    }
+    for(int i=temp.size()-1; i>=0; i--)
+    {
+        ans += temp[i];
+    }
+    cout << ans;
+    return 0;
+}
