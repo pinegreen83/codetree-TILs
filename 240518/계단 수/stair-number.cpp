b1d697ada4b73b06c084864228995412587ecd76
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-05-18 12:46:40 +0900

    Title: 계단 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240518/계단 수/stair-number.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    vector<vector<int>> dp(10, vector<int>(n));
+    for(int i=1; i<=9; i++) dp[i][0] = 1;
+
+    for(int i=1; i<n; i++)
+    {
+        for(int j=0; j<=9; j++)
+        {
+            if(j-1 >= 0)
+            {
+                dp[j][i] = dp[j-1][i-1];
+            }
+            if(j+1 <= 9)
+            {
+                dp[j][i] = (dp[j][i] + dp[j+1][i-1]) % 1000000007;
+            }
+        }
+    }
+
+    int ans = 0;
+    for(int i=0; i<=9; i++)
+    {
+        ans += dp[i][n-1];
+    }
+    cout << ans;
+
+    return 0;
+}
