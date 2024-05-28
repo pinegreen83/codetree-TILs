e3aaa260c0981e994a0156cc14374cfb2c6f41c3
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-05-28 21:36:50 +0900

    Title: 최대 이익 계산하기, Time: 24ms, Memory: 14MB, Status: Passed - CodeTree

--- 240528/최대 이익 계산하기/Calculate-maximum-profit.cpp
+++ 240528/최대 이익 계산하기/Calculate-maximum-profit.cpp
@@ -10,20 +10,27 @@ int main() {
     vector<int> nums(n);
     for(int i=0; i<n; i++) cin >> nums[i];
 
-    int cnt = 0, left = 0, right = n-1, ans = 0;
-    while(cnt < n)
+    vector<vector<int>> dp(n+1, vector<int>(n+1));
+    for(int i=1; i<=n; i++)
     {
-        cnt++;
-        if(nums[left] < nums[right]) ans += cnt * nums[left++];
-        else if(nums[left] > nums[right]) ans += cnt * nums[right--];
-        else
+        dp[i][0] = dp[i-1][0] + nums[i-1] * i;
+        dp[0][i] = dp[0][i-1] + nums[n-i] * i;
+    }
+
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n; j++)
         {
-            if(nums[left+1] > nums[right-1]) ans += cnt * nums[right--];
-            else ans += cnt * nums[left++];
+            if(i+j > n) break;
+            int k = i + j;
+            dp[i][j] = max(dp[i][j], dp[i][j-1] + k * nums[n - j]);
+            dp[i][j] = max(dp[i][j], dp[i-1][j] + k * nums[i-1]);
         }
     }
-    cout << ans;
 
+    int ans = 0;
+    for(int i=0; i<=n; i++) ans = max(ans, dp[i][n-i]);
+    cout << ans;
 
     return 0;
 }
