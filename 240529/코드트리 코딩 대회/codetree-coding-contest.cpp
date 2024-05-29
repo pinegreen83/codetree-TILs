2f9a7876617a8003253a9a75fb461e149f652277
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-05-29 22:47:41 +0900

    Title: 코드트리 코딩 대회, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240529/코드트리 코딩 대회/codetree-coding-contest.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int n, m, ans = INT_MAX;
+bool solve = true;
+vector<int> nums;
+
+void Find(int cnt, vector<int>& members)
+{
+    if(cnt == n)
+    {
+        ans = min(ans, (int)members.size());
+        return;
+    }
+    for(int i=0; i<members.size(); i++)
+    {
+        if(members[i] + nums[cnt] <= m)
+        {
+            members[i] += nums[cnt];
+            Find(cnt+1, members);
+            members[i] -= nums[cnt];
+        }
+    }
+
+    if(members.size() < ans)
+    {
+        members.push_back(nums[cnt]);
+        Find(cnt+1, members);
+        members.pop_back();
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m;
+    nums = vector<int>(n);
+    for(int i=0; i<n; i++) cin >> nums[i];
+
+    vector<int> members;
+    Find(0, members);
+
+    cout << ans;
+
+    return 0;
+}
