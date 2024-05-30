1369b81a5a0f52007b7f2518517d9db7384bdeb4
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-05-30 16:31:13 +0900

    Title: 연속된 가장 큰 합, Time: 32ms, Memory: 1MB, Status: Passed - CodeTree

--- 240530/연속된 가장 큰 합/largest-sum-in-a-row.cpp
+++ 240530/연속된 가장 큰 합/largest-sum-in-a-row.cpp
@@ -14,10 +14,8 @@ int main() {
     dp[1][0] = -10000;
     for(int i=1; i<=n; i++)
     {
-        if(dp[0][i-1] + nums[i-1] < 0) dp[0][i] = nums[i-1];
-        else dp[0][i] = dp[0][i-1] + nums[i-1];
-
-        dp[1][i] = max(dp[1][i-1]+nums[i-1], dp[0][i-1]);
+        dp[0][i] = max(dp[0][i-1] + nums[i-1], nums[i-1]);
+        dp[1][i] = max(dp[1][i-1] + nums[i-1], dp[0][i-1]);
     }
 
     int ans = 0;
