2364c9483ea53b7f8b91599c1a7ae6f244d8ba60
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-06-01 12:04:32 +0900

    Title: 십진수와 이진수, Time: 2ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240601/십진수와 이진수/decimal-and-binary-number.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string n;
+    cin >> n;
+    int s = n.size(), idx = 0;
+    vector<int> nums(s+5);
+    for(int i=s-1; i>=0; i--)
+    {
+        if(n[i] == '1')
+        {
+            nums[idx] += 1;
+            nums[idx+4] += 1;
+        }
+        if(nums[idx] > 1)
+        {
+            nums[idx] = 0;
+            nums[idx+1] += 1;
+        }
+        idx++;
+    }
+    if(nums[idx] > 1)
+    {
+        nums[idx] = 0;
+        nums[idx+1] += 1;
+    }
+
+    int start = 0;
+    if(nums[nums.size()-1] == 0) start = nums.size()-2;
+    else start = nums.size()-1;
+
+    for(int i=start; i>=0; i--) cout << nums[i];
+
+    return 0;
+}
