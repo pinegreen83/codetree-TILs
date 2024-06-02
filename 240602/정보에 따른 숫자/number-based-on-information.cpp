1b978d6b8a16ebc523373f44d54c17d2277976c8
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-06-02 17:55:05 +0900

    Title: 정보에 따른 숫자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240602/정보에 따른 숫자/number-based-on-information.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, a, b;
+    cin >> n >> a >> b;
+
+    vector<pair<int, int>> points;
+    points.push_back(make_pair(1, 0));
+    points.push_back(make_pair(1'000'000'000, 0));
+    for(int i=0; i<n; i++)
+    {
+        string info;
+        int num;
+        cin >> info >> num;
+        if(info == "S") points.push_back(make_pair(num, 1));
+        else points.push_back(make_pair(num, 0));
+    }
+    sort(points.begin(), points.end());
+
+    int ans = 0;
+    for(int i=0; i<n+1; i++)
+    {
+        pair<int, int> now = points[i];
+        pair<int, int> next = points[i+1];
+        
+        int start = now.first;
+        int end = next.first;
+        int mid = (start + end) / 2;
+
+        if(now.second == 1)
+        {
+            int s = max(a, start+1);
+            int e = min(b, mid);
+            if(e >= s) ans += e - s + 1;
+        }
+        if(next.second == 1)
+        {
+            int s = max(a, mid+1);
+            int e = min(b, end);
+            if(e >= s) ans += e - s + 1;
+            if(now.second == 0 && a <= mid && mid <= b) ans++;
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
