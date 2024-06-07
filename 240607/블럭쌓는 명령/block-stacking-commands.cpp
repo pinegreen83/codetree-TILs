ff1843c14fe6eb4fdfd82ee9e8facc8be5b41f43
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-06-07 18:48:27 +0900

    Title: 블럭쌓는 명령, Time: 71ms, Memory: 7MB, Status: Passed - CodeTree

---
+++ 240607/블럭쌓는 명령/block-stacking-commands.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, k;
+    cin >> n >> k;
+    vector<int> nums(n+2);
+    for(int i=0; i<k; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        nums[a] += 1;
+        if(b+1 <= n) nums[b+1] -= 1;
+    }
+
+    vector<int> ans(n);
+    ans[0] = nums[1];
+    for(int i=1; i<n; i++)
+    {
+        ans[i] = ans[i-1] + nums[i+1];
+    }
+
+    sort(ans.begin(), ans.end());
+    cout << ans[n/2];
+
+    return 0;
+}
