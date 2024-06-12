39f4e5b4f99bd7a9cee0e551d3cfe47d777b1616
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-06-12 15:22:45 +0900

    Title: 알파벳 쟁탈전, Time: 1037ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240612/알파벳 쟁탈전/alphabetics-competition.cpp
+++ 240612/알파벳 쟁탈전/alphabetics-competition.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int n, k, ans = 0;
-vector<int> same, diff;
+vector<vector<int>> same, diff;
 vector<char> comb, type = {'A', 'B', 'C'};
 
 void backtrack(int cnt)
@@ -14,47 +14,68 @@ void backtrack(int cnt)
         ans++;
         return;
     }
-    if(same[cnt] != 0 || diff[cnt] != 0)
+    for(int i=0; i<3; i++)
     {
-        if(same[cnt] != 0) 
+        comb[cnt] = type[i];
+        bool fine = true;
+        if(same[cnt].size() != 0)
         {
-            comb[cnt] = comb[same[cnt]];
-            backtrack(cnt+1);
-        }
-        else if(diff[cnt] != 0)
-        {
-            for(int i=0; i<3; i++)
+            for(int j=0; j<same[cnt].size(); j++)
             {
-                if(comb[diff[cnt]] == type[i]) continue;
-                comb[cnt] = type[i];
-                backtrack(cnt+1);
+                if(comb[same[cnt][j]] != ' ')
+                {
+                    if(comb[cnt] != comb[same[cnt][j]]) fine = false;
+                }
             }
         }
-    }
-    else
-    {
-        for(int i=0; i<3; i++)
+        if(diff[cnt].size() != 0)
         {
-            comb[cnt] = type[i];
-            backtrack(cnt+1);
+            for(int j=0; j<diff[cnt].size(); j++)
+            {
+                if(comb[cnt] == comb[diff[cnt][j]]) fine = false;
+            }
         }
+        if(fine) backtrack(cnt+1);
+        comb[cnt+1] = ' ';
     }
 }
 
 int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n >> k;
-    same = vector<int>(n+1);
-    diff = vector<int>(n+1);
-    comb = vector<char>(n+1);
+    same = vector<vector<int>>(n+1);
+    diff = vector<vector<int>>(n+1);
+    comb = vector<char>(n+1, ' ');
     for(int i=0; i<k; i++)
     {
         char c;
         int a, b;
         cin >> c >> a >> b;
-        if(c == 'S') a > b ? same[a] = b : same[b] = a;
-        else a > b ? diff[a] = b : diff[b] = a;
+        if(c == 'S')
+        {
+            same[a].push_back(b);
+            same[b].push_back(a);
+        }
+        else
+        {
+            diff[a].push_back(b);
+            diff[b].push_back(a);
+        }
     }
+
+    // for(int i=1; i<=n; i++)
+    // {
+    //     cout << i << " : ";
+    //     for(auto s : same[i]) cout << s << " ";
+    //     cout << "\n";
+    // }
+
+    // for(int i=1; i<=n; i++)
+    // {
+    //     cout << i << " : ";
+    //     for(auto s : diff[i]) cout << s << " ";
+    //     cout << "\n";
+    // }
 
     backtrack(1);
     cout << ans;
