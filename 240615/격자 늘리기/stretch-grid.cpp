7fdc42bbfdd43ab0897402f8696638e14bbd8f16
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-06-15 22:24:57 +0900

    Title: 격자 늘리기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240615/격자 늘리기/stretch-grid.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<vector<char>> map(n, vector<char>(m));
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++) cin >> map[i][j];
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        string now = "";
+        for(int j=0; j<m; j++)
+        {
+            now += map[i][j];
+            now += map[i][j];
+        }
+        now += '\n';
+        cout << now << now;
+    }
+
+    return 0;
+}
