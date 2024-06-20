a409d17c54f326dba93a7f73a27d9702a48de8dd
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-06-20 13:04:36 +0900

    Title: 원점으로 돌아오기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240620/원점으로 돌아오기/return-to-origin-point.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, ans = 0;
+vector<vector<int>> map;
+vector<bool> visited;
+
+void backtrack(int cnt, int x, int y)
+{
+    if(cnt == n)
+    {
+        if(x == 0 || y == 0) ans++;
+        return;
+    }
+    for(int i=0; i<n; i++)
+    {
+        if(!visited[i])
+        {
+            if(x == map[i][0] || y == map[i][1]) 
+            {
+                visited[i] = true;
+                backtrack(cnt+1, map[i][0], map[i][1]);
+                visited[i] = false;
+            }
+        }
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    cin >> n;
+    map = vector<vector<int>>(n, vector<int>(2));
+    for(int i=0; i<n; i++)
+    {
+        cin >> map[i][0] >> map[i][1];
+    }
+    visited = vector<bool>(n);
+    backtrack(0, 0, 0);
+    cout << ans;
+
+    return 0;
+}
