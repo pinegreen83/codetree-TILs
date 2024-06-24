87fcfd0d51033c996735cd0a949415284073ef32
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-06-24 22:08:45 +0900

    Title: T 제거하기, Time: 82ms, Memory: 2MB, Status: Passed - CodeTree

--- 240624/T 제거하기/remove-t.cpp
+++ 240624/T 제거하기/remove-t.cpp
@@ -3,37 +3,21 @@
 
 using namespace std;
 
-int tsize;
-string s, t;
-
-bool check()
-{
-    string ss = "";
-    for(int i=0; i<s.size(); i++)
-    {
-        string temp = s.substr(i, tsize);
-        if(temp == t)
-        {
-            string now = s.substr(i+tsize, s.size()-i);
-            ss += now;
-            swap(s, ss);
-            return true;
-        }
-        else ss += s[i];
-    }
-    return false;
-}
-
 int main() {
     // 여기에 코드를 작성해주세요.
+    string s, t;
     cin >> s >> t;
-    tsize = t.size();
+    int tsize = t.size();
+    
+    string ans = "";
+    for (char c : s) {
+        ans += c;
 
-    while(true)
-    {
-        if(!check()) break;
+        if (ans.size() >= tsize && ans.substr(ans.size() - tsize) == t) {
+            ans.erase(ans.size() - tsize);
+        }
     }
-    cout << s;
+    cout << ans;
 
     return 0;
 }
