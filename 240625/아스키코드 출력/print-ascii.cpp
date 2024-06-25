9c6f1cb3493a749ee256ab887cab09cb810230e8
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-06-25 15:05:39 +0900

    Title: 아스키코드 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240625/아스키코드 출력/print-ascii.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++)
+    {
+        int now;
+        cin >> now;
+        char ans = now;
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
