7f6014286b9f56b752774808adb949581811a463
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-06-29 17:20:50 +0900

    Title: 문자열 세번 출력하기, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240629/문자열 세번 출력하기/print-string-thrice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    std::string a;
+    std::cin >> a;
+    std::cout << a << "\n";
+    std::cout << a << "\n";
+    std::cout << a << '\n';
+    return 0;
+}
