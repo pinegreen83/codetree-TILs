3fe1e763c7975c0344a18e6c19a433b820559df0
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-07-03 14:10:49 +0900

    Title: 우유 생산량 경쟁, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240703/우유 생산량 경쟁/milk-production-competition.cpp
+++ 240703/우유 생산량 경쟁/milk-production-competition.cpp
@@ -17,6 +17,7 @@ int main() {
     cow_map["Elsie"] = 7;
     cow_map["Mildred"] = 7;
 
+    vector<string> names = {"Bessie", "Elsie", "Mildred"};
     vector<tuple<int, string, int>> daily;
     for(int i=0; i<n; i++)
     {
@@ -28,6 +29,7 @@ int main() {
 
     sort(daily.begin(), daily.end());
 
+    int change = 0;
     set<string> before;
     for(auto d : daily)
     {
@@ -36,8 +38,36 @@ int main() {
         tie(day, name, vol) = d;
         cow_map[name]+=vol;
 
-        cout << day << " " << name << " " << cow_map[name] << " " << vol << "\n";
+        vector<pair<int, string>> temp;
+        for(int i=0; i<3; i++)
+        {
+            temp.push_back({cow_map[names[i]], names[i]});
+        }
+
+        sort(temp.rbegin(), temp.rend());
+
+        set<string> now;
+        for(int i=0; i<2; i++)
+        {
+            if(temp[i].first == temp[i+1].first)
+            {
+                now.insert(temp[i].second);
+                now.insert(temp[i+1].second);
+            }
+            else
+            {
+                now.insert(temp[i].second);
+                break;
+            }
+        }
+        
+        if(before != now)
+        {
+            change++;
+            before = now;
+        }
     }
+    cout << change;
 
     return 0;
 }
