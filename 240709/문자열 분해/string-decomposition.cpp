da04238a5dd94e5d93f9b5b91b6124c76ec43606
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-07-09 13:42:44 +0900

    Title: 문자열 분해, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240709/문자열 분해/string-decomposition.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    cin >> str;
+
+    vector<bool> check(str.size());
+    int ans = 0;
+    bool finish = true;
+    while(finish)
+    {
+        int idx = 0, cnt = 0;
+        string now = "";
+        for(int i=0; i<str.size(); i++)
+        {
+            if(!check[i] && str[i] == 'a'+idx)
+            {
+                check[i] = true;
+                now += str[i];
+                idx++;
+                if(idx == 5) 
+                {
+                    idx = 0;
+                    cnt++;
+                }
+            }
+            else now += '_';
+        }
+        if(cnt == 0) finish = false;
+        else ans++;
+    }
+    cout << (ans == 0 ? -1 : ans);
+
+    return 0;
+}
