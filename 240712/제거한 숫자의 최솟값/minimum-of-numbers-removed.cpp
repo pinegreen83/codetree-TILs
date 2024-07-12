7169526151ef98264a240413243e3b77dbc28320
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-07-12 15:00:12 +0900

    Title: 제거한 숫자의 최솟값, Time: 14ms, Memory: 0MB, Status: Passed - CodeTree

--- 240712/제거한 숫자의 최솟값/minimum-of-numbers-removed.cpp
+++ 240712/제거한 숫자의 최솟값/minimum-of-numbers-removed.cpp
@@ -8,29 +8,29 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    vector<int> nums(n);
+    vector<long long> nums(n);
     for(int i=0; i<n; i++) cin >> nums[i];
 
     sort(nums.begin(), nums.end());
 
+    long long ans = 0, last = n-1;
     if(n%2 == 1)
     {
-        int ans = nums[n-1], last = n-2;
+        last--;
+        ans = nums[last];
         for(int i=0; i<n/2; i++)
         {
             ans = max(ans, nums[i] + nums[last-i]);
         }
-        cout << ans;
     }
     else
     {
-        int ans = 0, last = n-1;
         for(int i=0; i<n/2; i++)
         {
             ans = max(ans, nums[i] + nums[last-i]);
         }
-        cout << ans;
     }
+    cout << ans;
 
     return 0;
 }
