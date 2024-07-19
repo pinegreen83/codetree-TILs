58ea3539bbe825ec5bdf2f51a316a21964f0bef2
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-07-19 11:02:47 +0900

    Title: 앞의 두 수를 더한 수열 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240719/앞의 두 수를 더한 수열 2/a-sequence-by-adding-the-previous-two-numbers-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#define MAX_N 90
+
+using namespace std;
+
+int nums[MAX_N+1];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    nums[1] = 1;
+    nums[2] = 1;
+    int n;
+    cin >> n;
+
+    for(int i=3; i<=n; i++) nums[i] = nums[i-2] + nums[i-1];
+    cout << nums[n];
+
+    return 0;
+}
