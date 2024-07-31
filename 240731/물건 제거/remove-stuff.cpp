9a3e265972783841b49814c904c8f8d13c1ada4d
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-07-31 14:46:04 +0900

    Title: 물건 제거, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240731/물건 제거/remove-stuff.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    vector<int> machines(n);
+    for(int i=0; i<n; i++) cin >> machines[i];
+
+    int k;
+    cin >> k;
+    vector<int> products(k);
+    for(int i=0; i<k; i++) cin >> products[i];
+
+    int time = 0, totalCnt = 0;
+    vector<bool> visited(k);
+    while(totalCnt < k)
+    {
+        time++;
+        int cnt = 0;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<k; j++)
+            {
+                if(!visited[j] && products[j] <= machines[i])
+                {
+                    visited[j] = true;
+                    cnt++;
+                    break;
+                }
+            }
+        }
+        if(cnt == 0)
+        {
+            time = -1;
+            break;
+        }
+        else totalCnt += cnt;
+    }
+    cout << time;
+
+    return 0;
+}
