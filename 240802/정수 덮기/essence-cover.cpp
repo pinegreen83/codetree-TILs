0dd8cff106573e019b4b0110c6fefbe8cbe0927b
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-08-02 13:55:59 +0900

    Title: 정수 덮기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240802/정수 덮기/essence-cover.cpp
+++ 240802/정수 덮기/essence-cover.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-
-#define MAX_NUM 1000
+#include <vector>
+#include <algorithm>
 
 using namespace std;
-
-int lines[MAX_NUM+1];
 
 int main() {
     // 여기에 코드를 작성해주세요.
     int n, l;
     cin >> n >> l;
+    vector<int> lines(n);
     for(int i=0; i<n; i++) cin >> lines[i];
+
+    sort(lines.begin(), lines.end());
 
     float start = lines[0] - 0.5;
     float next = start + l;
