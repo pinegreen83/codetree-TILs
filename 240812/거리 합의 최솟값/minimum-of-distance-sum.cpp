6d2b22dfe0cdccf0b46ff55bb3094aa855e0eedd
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-08-12 13:31:31 +0900

    Title: 거리 합의 최솟값, Time: 11ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240812/거리 합의 최솟값/minimum-of-distance-sum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    vector<int> xs(n), ys(n);
+    for(int i=0; i<n; i++) cin >> xs[i] >> ys[i];
+
+    sort(xs.begin(), xs.end());
+    sort(ys.begin(), ys.end());
+
+    int midx = xs[n/2], midy = ys[n/2];
+
+    int sumx = 0, sumy = 0;
+    for(int i=0; i<n; i++)
+    {
+        sumx += abs(xs[i] - midx);
+        sumy += abs(ys[i] - midy);
+    }
+    cout << sumx + sumy;
+
+    return 0;
+}
