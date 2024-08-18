d0792e147f47c6011b0bb5a13a07fa4d95605b33
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-08-18 15:43:07 +0900

    Title: 나선형 삼각형, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240818/나선형 삼각형/spira-triangle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    int dp[101];
+    dp[1] = 1;
+    dp[2] = 1;
+    dp[3] = 1;
+    dp[4] = 2;
+    dp[5] = 2;
+    for(int i=6; i<=n; i++) {
+        dp[i] = dp[i-5] + dp[i-1];
+    }
+    cout << dp[n];
+
+    return 0;
+}
