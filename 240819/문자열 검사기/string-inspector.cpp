944aff2daf27b09b4a9adb4e96213094db1e7595
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-08-19 15:19:51 +0900

    Title: 문자열 검사기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240819/문자열 검사기/string-inspector.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    cin >> str;
+
+    int ans = 1;
+    vector<char> stack;
+
+    for(int i=0; i<str.size(); i++) {
+        if(str[i] == '(' || str[i] == '[') {
+            stack.push_back(str[i]);
+        }
+        else if(str[i] == ')') {
+            if(stack.back() == '(') stack.pop_back();
+            else {
+                ans = 1;
+                break;
+            }
+        }
+        else if(str[i] == ']') {
+            if(stack.back() == '[') stack.pop_back();
+            else {
+                ans = 1;
+                break;
+            }
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
