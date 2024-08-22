94a7a874f7c17924ebc9f401b7eb8ab5c8c257c9
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-08-22 14:02:06 +0900

    Title: 매칭하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240822/매칭하기/matching.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, ans = 0;
+    cin >> n;
+
+    string str;
+    for(int i=0; i<n; i++) {
+        cin >> str;
+        vector<char> stack;
+
+        for(int j=0; j<str.size(); j++) {
+            if(stack.empty() || stack.back() != str[j]) {
+                stack.push_back(str[i]);
+            }
+            else if(stack.back() == str[i]) {
+                stack.pop_back();
+            }
+        }
+
+        if(stack.empty()) ans++;
+    }
+    cout << ans;
+
+    return 0;
+}
