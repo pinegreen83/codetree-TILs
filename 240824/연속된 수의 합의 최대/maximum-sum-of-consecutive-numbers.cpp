a32e4db26a8723ccefa872a59e5c89c4db6c9509
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-08-24 16:27:13 +0900

    Title: 연속된 수의 합의 최대, Time: 1097ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

--- 240824/연속된 수의 합의 최대/maximum-sum-of-consecutive-numbers.cpp
+++ 240824/연속된 수의 합의 최대/maximum-sum-of-consecutive-numbers.cpp
@@ -12,13 +12,13 @@ int main() {
     for (int i = 0; i < n; i++) cin >> nums[i];
 
     vector<long long> prefixSum(n+1);
-    for (int i = 1; i <= n; i++) {
+    for (int i=1; i<=n; i++) {
         prefixSum[i] = prefixSum[i-1] + nums[i-1];
     }
 
     long long maxSum = -100000000;
-    for (int i = k + 1; i <= n; i++) {
-        for (int j = 0; j <= i - k; j++) {
+    for (int i=k; i<=n; i++) {
+        for (int j=0; j<=i-k; j++) {
             maxSum = max(maxSum, prefixSum[i] - prefixSum[j]);
         }
     }
