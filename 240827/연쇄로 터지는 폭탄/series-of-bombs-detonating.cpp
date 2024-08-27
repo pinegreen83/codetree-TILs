b86e000082b2ed13efdd206d0fc40a61b1622c5b
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-08-27 15:10:23 +0900

    Title: 연쇄로 터지는 폭탄, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240827/연쇄로 터지는 폭탄/series-of-bombs-detonating.cpp
+++ 240827/연쇄로 터지는 폭탄/series-of-bombs-detonating.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
@@ -15,31 +16,29 @@ int main() {
 
     int ans = 0;
     for(int i=0; i<n; i++) {
-        int cnt = 1, idx = 0;
+        vector<bool> visited(n, false);
+        queue<pair<int, int>> q;
+        q.push({i, 1});
+        visited[i] = true;
+        int cnt = 0;
+        while(!q.empty()) {
+            int now = q.front().first;
+            int range = q.front().second;
+            q.pop();
+            cnt++;
 
-        // 시작 지점의 좌, 우를 확인해서 범위 안에 있으면 터트리고 터트린 방향으로 쭉 나아감.
-        bool left = true, right = true;
-        while(true) {
-            idx++;
-            if(left) {
-                if(i - idx < 0) left = false;
-                else {
-                    if(nums[i-idx] >= nums[i-idx+1]-idx) {
-                        cnt++;
-                    }
-                    else left = false;
+            for(int i=now-1; i>=0 && nums[now] - nums[i] <= range; i--) {
+                if(!visited[i]) {
+                    visited[i] = true;
+                    q.push({i, range+1});
                 }
             }
-            if(right) {
-                if(i + idx >= n) right = false;
-                else {
-                    if(nums[i+idx] <= nums[i+idx-1]+idx) {
-                        cnt++;
-                    }
-                    else right = false;
+            for(int i=now+1; i<n && nums[i] - nums[now] <= range; i++) {
+                if(!visited[i]) {
+                    visited[i] = true;
+                    q.push({i, range+1});
                 }
             }
-            if(!left && !right) break;
         }
         ans = max(ans, cnt);
     }
