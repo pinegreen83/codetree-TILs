d10403d7ac477b7fa5b05f96ad1e9db6843de1ac
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-09-04 13:50:35 +0900

    Title: 오목의 승패, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240904/오목의 승패/concave-victory-and-defeat.cpp
+++ 240904/오목의 승패/concave-victory-and-defeat.cpp
@@ -1,29 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int ans = 0, ansx, ansy;
-int map[20][20], dirs[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
-bool visited[20][20] = {};
+int map[20][20], dirs[3][2] = {{-1, 1}, {0, 1}, {1, 1}};
 
 bool isin(int x, int y) {
     return 1 <= x && x <= 19 && 1 <= y && y <= 19;
-}
-
-void dfs(int dir, int cnt, int x, int y, int type) {
-    int dirx = x + dirs[dir][0];
-    int diry = y + dirs[dir][1];
-
-    if(isin(dirx, diry) && !visited[dirx][diry] && map[dirx][diry] == type) {
-        dfs(dir, cnt+1, dirx, diry, type);
-    }
-    else {
-        if(cnt == 5) {
-            ans = type;
-            ansx = dirx - dirs[dir][0] * 5;
-            ansy = diry - dirs[dir][1] * 5;
-        }
-        return;
-    }
 }
 
 int main() {
@@ -33,12 +14,29 @@ int main() {
         for(int j=1; j<=19; j++) cin >> map[i][j];
     }
 
+    int dirx, diry, cnt, ans = 0, ansx, ansy;
+    bool find = false;
     for(int i=1; i<=19; i++) {
+        if(find) break;
         for(int j=1; j<=19; j++) {
-            if(map[i][j] != 0 && !visited[i][j]) {
-                visited[i][j] = true;
-                for(int d=0; d<8; d++) {
-                    dfs(d, 1, i, j, map[i][j]);
+            if(find) break;
+            if(map[i][j] != 0) {
+                for(int d=0; d<3; d++) {
+                    dirx = i + dirs[d][0];
+                    diry = j + dirs[d][1];
+                    cnt = 1;
+
+                    while(isin(dirx, diry) && map[i][j] == map[dirx][diry]) {
+                        cnt++;
+                        dirx += dirs[d][0];
+                        diry += dirs[d][1];
+                    }
+                    if(cnt == 5) {
+                        find = true;
+                        ans = map[i][j];
+                        ansx = i;
+                        ansy = j;
+                    }
                 }
             }
         }
