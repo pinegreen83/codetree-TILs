407e3d548e5fcac35dd8e1dcecc99b0298800180
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-09-06 14:45:38 +0900

    Title: 연달아 출력하는 재귀함수, Time: 243ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240906/연달아 출력하는 재귀함수/recursive-function-that-outputs-continuously.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void recur(int now) {
+    if(now == 1) {
+        cout << now << " ";
+        return;
+    }
+    recur(now / 3);
+    cout << now << " ";
+    return;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    recur(n);
+
+    return 0;
+}
