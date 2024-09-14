ffc530924db21a5382203d764e4e912b545b09fb
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-09-14 17:44:55 +0900

    Title: 숫자의 문자화, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240914/숫자의 문자화/characterization-of-numbers.cpp
+++ 240914/숫자의 문자화/characterization-of-numbers.cpp
@@ -14,13 +14,15 @@ int main() {
     vector<int> dp(n+1, 0);
     dp[0] = 1;
     for(int i=1; i<=n; i++) {
-        dp[i] += dp[i-1];
-        if(str[i-1] == '1') {
-            dp[i+1] += dp[i-1];
+        if(str[i-1] != '0') dp[i] += dp[i-1];
+
+        if(i > 1) {
+            int num = (str[i-2] - '0') * 10 + (str[i-1] - '0');
+            if(10 <= num && num <= 26) {
+                dp[i] += dp[i-2];
+            }
         }
-        else if(str[i-1] == '2') {
-            if(i < n-1 && '0' <= str[i] && str[i] < '7') dp[i+1] += dp[i-1];
-        }
+
         dp[i] %= 10007;
     }
     cout << dp[n] % 10007;
