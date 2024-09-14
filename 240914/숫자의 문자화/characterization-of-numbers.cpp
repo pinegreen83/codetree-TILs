a723bd9445d8e2267956187b386d57d6a8bcc480
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-09-14 17:40:31 +0900

    Title: 숫자의 문자화, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240914/숫자의 문자화/characterization-of-numbers.cpp
+++ 240914/숫자의 문자화/characterization-of-numbers.cpp
@@ -1,41 +1,29 @@
 #include <iostream>
-#include <set>
+#include <vector>
 
 using namespace std;
 
 int n;
 string str;
-set<string> ss;
-
-void Backtrack(int cnt, string now) {
-    if(cnt == n) {
-        ss.insert(now);
-        return;
-    }
-
-    if(str[cnt] == '1') {
-        if(cnt < n) {
-            char temp = 'A' + ((str[cnt] - '0') * 10 + (str[cnt+1] - '1'));
-            Backtrack(cnt + 2, now + temp);
-        }
-    }
-    else if(str[cnt] == '2') {
-        if(cnt < n) {
-            char temp = 'A' + ((str[cnt] - '0') * 10 + (str[cnt+1] - '1'));
-            Backtrack(cnt + 2, now + temp);
-        }
-    }
-    char temp = 'A' + str[cnt] - '1';
-    Backtrack(cnt + 1, now + temp);
-}
 
 int main() {
     // 여기에 코드를 작성해주세요.
     cin >> str;
 
     n = str.size();
-    Backtrack(0, "");
-    cout << ss.size() % 10007;
+    vector<int> dp(n+1, 0);
+    dp[0] = 1;
+    for(int i=1; i<=n; i++) {
+        dp[i] += dp[i-1];
+        if(str[i-1] == '1') {
+            dp[i+1] += dp[i-1];
+        }
+        else if(str[i-1] == '2') {
+            if(i < n-1 && '0' <= str[i] && str[i] < '7') dp[i+1] += dp[i-1];
+        }
+        dp[i] %= 10007;
+    }
+    cout << dp[n] % 10007;
 
     return 0;
 }
