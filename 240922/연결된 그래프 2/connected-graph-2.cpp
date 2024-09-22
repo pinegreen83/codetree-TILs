8e5b86813099160e73f5b560492c23eefb9c1120
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-09-22 17:47:04 +0900

    Title: 연결된 그래프 2, Time: 86ms, Memory: 0MB, Status: Passed - CodeTree

--- 240922/연결된 그래프 2/connected-graph-2.cpp
+++ 240922/연결된 그래프 2/connected-graph-2.cpp
@@ -4,26 +4,19 @@
 using namespace std;
 
 bool check;
-int n, m, dist = 0;
-vector<int> ans;
+int n, m, cnt;
+vector<int> counts;
 vector<bool> visited;
 vector<vector<int>> map;
 
-void DFS(int cnt, int now) {
-    if(cnt >= dist) {
-        check = true;
-        if(cnt > dist) {
-            ans.clear();
-            dist = cnt;
-        }
-    }
-
+void DFS(int now) {
     for(int i=0; i<map[now].size(); i++) {
         int next = map[now][i];
 
         if(!visited[next]) {
             visited[next] = true;
-            DFS(cnt+1, next);
+            cnt++;
+            DFS(next);
         }
     }
 }
@@ -44,11 +37,20 @@ int main() {
         visited = vector<bool>(n+1, false);
         visited[i] = true;
 
-        check = false;
-        DFS(0, i);
-        if(check) {
-            ans.push_back(i);
+        cnt = 0;
+        DFS(i);
+        counts.push_back(cnt);
+    }
+
+    int dist = 0;
+    vector<int> ans;
+    for(int i=0; i<n; i++) {
+        if(dist < counts[i]) {
+            dist = counts[i];
+            ans.clear();
+            ans.push_back(i+1);
         }
+        else if(dist == counts[i]) ans.push_back(i+1);
     }
 
     for(auto& a : ans) cout << a << " ";
