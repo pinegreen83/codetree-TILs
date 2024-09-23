a2cd46cbfa9bf73a1ee1e06ddd23e2a5408f8ebf
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-09-23 15:55:47 +0900

    Title: n x m 표 이동 9, Time: 363ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240923/n x m 표 이동 9/move-n-x-m-table-9.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int n, m;
+
+struct Node {
+    int x;
+    int y;
+    bool check;
+    int cnt;
+};
+
+bool isin(int x, int y) {
+    return 0 <= x && x < n && 0 <= y && y < m;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m;
+
+    int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    vector<vector<int>> map(n, vector<int>(m));
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) cin >> map[i][j];
+    }
+
+    queue<Node> q;
+    if(map[0][0]) q.push(Node{0, 0, true, 1});
+    else q.push(Node{0, 0, false, 1});
+
+    int ans = -1;
+    while(!q.empty()) {
+        Node now = q.front();
+        q.pop();
+
+        if(now.x == n-1 && now.y == m-1) {
+            ans = now.cnt;
+            break;
+        }
+
+        for(int d=0; d<4; d++) {
+            int dirx = now.x + dirs[d][0];
+            int diry = now.y + dirs[d][1];
+
+            if(isin(dirx, diry)) {
+                if(now.check) {
+                    if(map[dirx][diry] == 0) {
+                        q.push(Node{dirx, diry, true, now.cnt+1});
+                    }
+                }
+                else {
+                    if(map[dirx][diry] == 1) {
+                        q.push(Node{dirx, diry, true, now.cnt+1});
+                    }
+                    else q.push(Node{dirx, diry, false, now.cnt+1});
+                }
+            }
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
