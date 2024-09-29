4fc5e37014b71ad1059bac3f4c2ae237bc780a18
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-09-29 19:25:34 +0900

    Title: 세 수의 합과 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240929/세 수의 합과 곱/sum-and-product-of-three-numbers.cpp
+++ 240929/세 수의 합과 곱/sum-and-product-of-three-numbers.cpp
@@ -5,7 +5,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b, c;
     cin >> a >> b >> c;
-    cout << (a+b+c) << "\n" << (a*b*c);
+    cout << "sum = " << (a+b+c) << "\nmul = " << (a*b*c);
 
     return 0;
 }
