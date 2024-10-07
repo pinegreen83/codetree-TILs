e8b98d8bda72c1d6aeef62d75ead970071d7ef0c
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-10-07 19:13:18 +0900

    Title: 가장 큰 정사각형의 크기, Time: 16ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241007/가장 큰 정사각형의 크기/size-of-the-largest-square.cpp
+++ 241007/가장 큰 정사각형의 크기/size-of-the-largest-square.cpp
@@ -25,14 +25,18 @@ int main() {
                 }
                 dp1[i][j] = cnt;
             }
+        }
+    }
 
-            if(map[j][i] == 1) {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            if(map[i][j] == 1) {
                 int cnt = 1;
-                for(int k=j+1; k<n; k++) {
-                    if(map[k][i] == 1) cnt++;
+                for(int k=i+1; k<n; k++) {
+                    if(map[k][j] == 1) cnt++;
                     else break;
                 }
-                dp2[j][i] = cnt;
+                dp2[i][j] = cnt;
             }
         }
     }
