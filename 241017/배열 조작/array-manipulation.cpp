15912f877282797865d4d27ce7fb0cf94546b6e6
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-10-17 11:56:25 +0900

    Title: 배열 조작, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241017/배열 조작/array-manipulation.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    string opers;
+    cin >> opers;
+
+    int n;
+    cin >> n;
+
+    deque<int> deq;
+    for(int i=0; i<n; i++) {
+        int now;
+        cin >> now;
+        deq.push_back(now);
+    }
+
+    bool isReverse = false;
+    for(auto oper : opers) {
+        if(oper == 'R') {
+            if(isReverse) isReverse = false;
+            else isReverse = true;
+        }
+        else {
+            if(isReverse) {
+                deq.pop_back();
+            }
+            else deq.pop_front();
+        }
+    }
+
+    if(isReverse) {
+        for(auto i=deq.rbegin(); i!=deq.rend(); i++) cout << *i << ' ';
+    }
+    else {
+        for(auto i=deq.begin(); i!=deq.end(); i++) cout << *i << ' ';
+    }
+
+    return 0;
+}
