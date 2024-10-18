347475c10647b2b518d6a5ed88d65b65e6eaa3c9
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-10-18 22:23:45 +0900

    Title: 문자열을 출력하는 재귀함수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241018/문자열을 출력하는 재귀함수/recursive-function-that-outputs-string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    cin >> n;
+
+    while(n--) {
+        cout << "Coding is my favorite hobby!\n";
+    }
+
+    return 0;
+}
