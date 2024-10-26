d2ff269a38c67a17eb25e6db81d7128e8e22392c
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-10-26 22:09:32 +0900

    Title: 문자열 이어붙이기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241026/문자열 이어붙이기/string-pasting.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string a, b;
+    cin >> a >> b;
+
+    for(int i=0; i<3; i++) cout << b[i];
+    cout << a;
+
+    return 0;
+}
