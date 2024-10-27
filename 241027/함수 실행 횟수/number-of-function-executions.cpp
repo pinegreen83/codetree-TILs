6452f1c0da53a3ca20872feecca7120327c7c689
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-10-27 21:44:08 +0900

    Title: 함수 실행 횟수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241027/함수 실행 횟수/number-of-function-executions.cpp
+++ 241027/함수 실행 횟수/number-of-function-executions.cpp
@@ -8,12 +8,12 @@ int main() {
     int n;
     cin >> n;
 
-    vector<int> dp(n+1, 0);
+    vector<int> dp(n+1, 1);
     dp[0] = 1;
-    dp[1] = 2;
+    dp[1] = 1;
 
     for(int i=2; i<=n; i++) {
-        dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+        dp[i] += (dp[i-1] + dp[i-2]) % 1000000007;
     }
     cout << dp[n];
 
