d4b3823617bfaf850e3fd82fcdbf8cd39ad73c98
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-11-01 17:07:15 +0900

    Title: 개발자의 취준생활, Time: 1035ms, Memory: 8MB, Status: Time Limit Exceed - CodeTree

--- 241101/개발자의 취준생활/developers-employment-life.cpp
+++ 241101/개발자의 취준생활/developers-employment-life.cpp
@@ -9,25 +9,24 @@ int main() {
     int n;
     cin >> n;
 
-    vector<int> want(n), maxBill(n);
+    vector<int> want(n), maxBill(n), dp(1 << n, 0);
     for(int i=0; i<n; i++) cin >> want[i];
     for(int i=0; i<n; i++) cin >> maxBill[i];
 
-    vector<vector<int>> dp(n+1, vector<int>(1 << n, 0));
-    dp[0][0] = 1;
+    dp[0] = 1;
     
     for(int i=0; i<n; i++) {
-        for(int j=0; j<(1<<n); j++) {
-            if(dp[i][j] == 0) continue;
-
+        vector<int> next = dp;
+        for(int j=0; j<(1 << n); j++) {
             for(int k=0; k<n; k++) {
-                if(!(j & (1 << k)) && want[i] <= maxBill[k]) {
-                    dp[i+1][j | (1 << k)] += dp[i][j];
+                if((j & (1 << k)) == 0 && maxBill[i] >= want[k]) {
+                    next[j | (1 << k)] += dp[j];
                 }
             }
         }
+        dp = next;
     }
-    cout << dp[n][(1 << n) - 1];
+    cout << dp[(1 << n) - 1];
 
     return 0;
 }
