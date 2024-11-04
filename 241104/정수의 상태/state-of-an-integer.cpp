76531ec1f60fb329c3ad446bc602ca9064938659
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-11-04 18:09:07 +0900

    Title: 정수의 상태, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241104/정수의 상태/state-of-an-integer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int now;
+    cin >> now;
+    cout << (now >= 0 ? (now > 0 ? "positive" : "zero") : "negative");
+
+    return 0;
+}
