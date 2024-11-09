5e5893bc4a2c37b454fdc166cebcf98ae8d147c3
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-11-09 22:39:58 +0900

    Title: 산책, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241109/산책/walk.cpp
+++ 241109/산책/walk.cpp
@@ -4,26 +4,6 @@
 
 using namespace std;
 
-int ans = 0;
-vector<int> comb;
-
-void DFS(int idx, vector<int>& clothes) {
-    if(idx == clothes.size()) {
-        int cnt = 0;
-        for(int i=0; i<clothes.size(); i++) {
-            if(comb[i] != 0) cnt++;
-        }
-        if(cnt != 0) ans++;
-        return;
-    }
-
-    for(int i=0; i<clothes[idx]; i++) {
-        comb.push_back(i);
-        DFS(idx+1, clothes);
-        comb.pop_back();
-    }
-}
-
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
@@ -42,13 +22,12 @@ int main() {
         else umap[b].push_back(a);
     }
 
-    vector<int> clothes;
+    long long ans = 1;
     for(auto um : umap) {
-        clothes.push_back(um.second.size());
+        ans *= um.second.size();
     }
 
-    DFS(0, clothes);
-    cout << ans;
+    cout << ans - 1;
 
     return 0;
 }
