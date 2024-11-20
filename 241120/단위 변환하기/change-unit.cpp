c021b6f83c77e59070cf1c62f0d1aa7fa29bcd3d
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-11-20 13:49:09 +0900

    Title: 단위 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241120/단위 변환하기/change-unit.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    printf("2.80 mi = %.2f\n", (double) 2.80 * 160934.40);
+    printf("128.40 ft = %.2f", (double) 128.40 * 30.48);
+
+    return 0;
+}
