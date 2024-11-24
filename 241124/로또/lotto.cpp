27e0574bf3cc3bbe8eff8a10690d83662fbe380e
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-11-24 14:55:00 +0900

    Title: 로또, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241124/로또/lotto.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+vector<int> nums, combs;
+
+void Backtrack(int cnt, int start) {
+    if(cnt == 6) {
+        for(const auto comb : combs) cout << comb << " ";
+        cout << "\n";
+        return;
+    }
+
+    for(int i=start; i<n; i++) {
+        combs.push_back(nums[i]);
+        Backtrack(cnt+1, i+1);
+        combs.pop_back();
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    nums = vector<int>(n);
+    for(int i=0; i<n; i++) cin >> nums[i];
+
+    Backtrack(0, 0);
+
+    return 0;
+}
