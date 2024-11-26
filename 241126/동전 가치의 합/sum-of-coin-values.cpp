68730ea1d1f3a36c213cec9fe4fa6f987de92e27
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-11-26 15:15:04 +0900

    Title: 동전 가치의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241126/동전 가치의 합/sum-of-coin-values.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> coins(n);
+    vector<int> costs(m+1, 0);
+    for(int i=0; i<n; i++) {
+        cin >> coins[i];
+    }
+
+    costs[0] = 1;
+    for(int j=0; j<n; j++) {
+        for(int i=1; i<=m; i++) {
+            costs[i] = (costs[i] + costs[i-coins[j]]) % 1000000007;
+        }
+    }
+    cout << (costs[m] == 0 ? -1 : costs[m]);
+
+    return 0;
+}
