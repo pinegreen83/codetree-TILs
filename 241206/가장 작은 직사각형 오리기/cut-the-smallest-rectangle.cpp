003c90db3153376f23bbd0df406e59a485a77acc
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-12-06 17:17:51 +0900

    Title: 가장 작은 직사각형 오리기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241206/가장 작은 직사각형 오리기/cut-the-smallest-rectangle.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    vector<int> nums(301, 0);
+
+    while(n--) {
+        int s, e;
+        cin >> s >> e;
+
+        for(int i=s; i<=e; i++) {
+            nums[i]++;
+        }
+    }
+
+    int ans = 0;
+
+    int cnt = 0, maxH = 0;
+    for(int i=1; i<=300; i++) {
+        if(nums[i] != 0) {
+            cnt++;
+            maxH = max(maxH, nums[i]);
+        }
+        else {
+            ans += cnt * maxH;
+            cnt = 0;
+            maxH = 0;
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
