a0e47a169495c91b15a1076f3e973d37cc3c2b01
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-12-16 13:44:06 +0900

    Title: 특정 합을 위한 최소 비용, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241216/특정 합을 위한 최소 비용/minimum-cost-for-a-specific-sum.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <climits>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> nums(n);
+    for(int i=0; i<n; i++) cin >> nums[i];
+
+    vector<vector<int>> dp(n+1, vector<int>(m+1, INT_MAX));
+    dp[0][0] = 0;
+
+    int maxN = sqrt(m);
+
+    for(int i=1; i<=n; i++) {
+        int num = nums[i-1];
+        for(int j=0; j<=m; j++) {
+            if(dp[i-1][j] < INT_MAX) {
+                for(int k=1; k<=maxN+1; k++) {
+                    int sqrts = k * k;
+                    if(j + sqrts <= m) {
+                        int cost = (num - k) * (num - k);
+                        dp[i][j+sqrts] = min(dp[i][j + sqrts], dp[i-1][j] + cost);
+                    }
+                }
+            }
+        }
+    }
+    cout << dp[n][m];
+
+    return 0;
+}
