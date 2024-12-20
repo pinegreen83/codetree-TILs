05e8e2e9c52fac8116546b6cdff5e0de135b3618
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-12-20 15:41:08 +0900

    Title: 6가지 관계연산자, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241220/6가지 관계연산자/six-relational-operators.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+    cout << (a >= b) << "\n" << (a > b) << "\n" << (b >= a) << "\n" << (b > a) << "\n" << (a == b) << "\n" << (a != b);
+    return 0;
+}
