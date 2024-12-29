f615b65737b5e7eb827795d0a112930b54b759c9
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2024-12-29 16:46:09 +0900

    Title: 실수와 문자 받아 출력하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241229/실수와 문자 받아 출력하기 2/print-number--and-letter-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char s;
+    double x, y;
+    cin >> s >> x >> y;
+
+    printf("%.2f\n%.2f\n%c", y, x, s);    
+
+    return 0;
+}
