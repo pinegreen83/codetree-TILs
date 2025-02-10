c3fb23880f2ab956f20c03319d8a0c0a9052b3ba
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2025-02-10 20:38:50 +0900

    Title: 올바른 등식 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250210/올바른 등식 만들기/right-equality.cpp
+++ 250210/올바른 등식 만들기/right-equality.cpp
@@ -15,17 +15,18 @@ int main() {
 
     // Write your code here!
     vector<vector<long long>> dp(N, vector<long long>(41, 0));
-    dp[0][nums[0] + 20] = 1;
-    dp[0][-nums[0] + 20] = 1;
+    dp[0][nums[0] + 20] += 1;
+    dp[0][-nums[0] + 20] += 1;
 
     for(int i=1; i<N; i++) {
         for(int j=-20; j<=20; j++) {
             if(dp[i-1][j+20] > 0) {
                 int pnow = j + nums[i];
-                int mnow = j - nums[i];
                 if(pnow >= -20 && pnow <= 20) {
                     dp[i][pnow + 20] += dp[i-1][j + 20];
                 }
+
+                int mnow = j - nums[i];
                 if(mnow >= -20 && mnow <= 20) {
                     dp[i][mnow + 20] += dp[i-1][j + 20];
                 }
