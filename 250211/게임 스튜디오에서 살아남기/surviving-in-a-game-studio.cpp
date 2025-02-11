0c0da36d9e82ade91d58c9838156c1720cb3ac6e
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2025-02-11 22:32:40 +0900

    Title: 게임 스튜디오에서 살아남기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250211/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.cpp
+++ 250211/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.cpp
@@ -11,24 +11,23 @@ int main() {
     cin >> n;
 
     // Write your code here!
-    vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(3, vector<int>(3, 0)));
+    vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(5, vector<int>(5, 0)));
     dp[1][0][0] = 1;
     dp[1][1][0] = 1;
     dp[1][0][1] = 1;
 
-    for(int i=2; i<=n; i++) {
-        for(int t=0; t<=2; t++) {
-            dp[i][t][0] = (dp[i-1][t][0] + dp[i-1][t][1] + dp[i-1][t][2] + dp[i-1][t][3]) % Mod;
-
-            dp[i][t][1] = dp[i-1][t][0];
-            dp[i][t][2] = dp[i-1][t][1];
-
-            if(t > 0) dp[i][t][0] = (dp[i][t][0] + dp[i-1][t-1][0] + dp[i-1][t-1][1] + dp[i-1][t-1][2]) % Mod;
+    for(int i=1; i<n; i++) {
+        for(int t=0; t<3; t++) {
+            for(int j=0; j<3; j++) {
+                dp[i+1][t+1][0] = (dp[i+1][t+1][0] + dp[i][t][j]) % Mod;
+                dp[i+1][t][0] = (dp[i+1][t][0] + dp[i][t][j]) % Mod;
+                dp[i+1][t][j+1] = (dp[i+1][t][j+1] + dp[i][t][j]) % Mod;
+            }
         }
     }
     
     int ans = 0;
-    for(int t=0; t<=2; t++) {
+    for(int t=0; t<3; t++) {
         for(int j=0; j<3; j++) {
             ans = (ans + dp[n][t][j]) % Mod;
         }
