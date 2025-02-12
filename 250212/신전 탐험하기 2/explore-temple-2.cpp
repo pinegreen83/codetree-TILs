aecf9c5258b0beb047518477d9d09e346b93eeee
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2025-02-12 13:36:18 +0900

    Title: 신전 탐험하기 2, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 250212/신전 탐험하기 2/explore-temple-2.cpp
+++ 250212/신전 탐험하기 2/explore-temple-2.cpp
@@ -14,18 +14,31 @@ int main() {
     }
 
     // Write your code here!
-    vector<vector<int>> dp(n+1, vector<int>(3, 0));
-
-    for(int i=2; i<=n; i++) {
-        dp[i][0] = max(dp[i-1][1], dp[i-1][2]) + l[i-1];
-        dp[i][1] = max(dp[i-1][0], dp[i-1][2]) + m[i-1];
-        dp[i][2] = max(dp[i-1][0], dp[i-1][1]) + r[i-1];
-    }
-
     int ans = 0;
-    ans = max(ans, (max(dp[n][1], dp[n][2]) + l[0]));
-    ans = max(ans, (max(dp[n][0], dp[n][2]) + m[0]));
-    ans = max(ans, (max(dp[n][0], dp[n][1]) + r[0]));
+
+    for(int s=0; s<3; s++) {   
+        vector<vector<int>> dp(n+1, vector<int>(3, -1e9));
+
+        if(s == 0) {
+            dp[1][0] = l[0]; 
+        }
+        else if(s == 1) {
+            dp[1][1] = m[0];
+        }
+        else {
+            dp[1][2] = r[0];
+        }
+
+        for(int i=2; i<=n; i++) {
+            dp[i][0] = max(dp[i-1][1], dp[i-1][2]) + l[i-1];
+            dp[i][1] = max(dp[i-1][0], dp[i-1][2]) + m[i-1];
+            dp[i][2] = max(dp[i-1][0], dp[i-1][1]) + r[i-1];
+        }
+
+        if(s == 0) ans = max(ans, max(dp[n][1], dp[n][2]));
+        if(s == 1) ans = max(ans, max(dp[n][0], dp[n][2]));
+        if(s == 2) ans = max(ans, max(dp[n][0], dp[n][1]));
+    }
     cout << ans;
 
     return 0;
